37a2805015b2fa403e7d09e45c20c8658b9f6dff
Lavanov <alavanov@rencap.com>
2022-09-16 15:38:03 +0300

    Odd or not

---
+++ Odd or not/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число:");
+int number=Math.Abs(Convert.ToInt32(ReadLine()));
+
+string result = "Число четное";
+if (number%2==1) result = "Числое нечетное";
+
+WriteLine(result);
